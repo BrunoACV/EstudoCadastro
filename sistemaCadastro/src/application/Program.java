24b60fe9c81ef627b5c9d28f6a12537c6769ddb2
Bruno <brunovieirabow@gmail.com>
2021-08-05 22:51:57 -0300

    primeiro passo comcluído.

---
+++ sistemaCadastro/src/application/Program.java
@@ -0,0 +1,39 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import entities.Usuario;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Usuario> usuario = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		String senha;
+		String senhaVal;
+		System.out.println("Bem vindo! ");
+
+		System.out.print("Informe o seu nome: ");
+		String nome = sc.nextLine();
+		System.out.print("Informe o seu Email: ");
+		String email = sc.nextLine();
+
+		System.out.print("Digite a sua senha: ");
+		senha = sc.nextLine();
+		System.out.print("Confirme a senha: ");
+		senhaVal = sc.nextLine();
+
+		if (senhaVal.equals(senha)) {
+			usuario.add(new Usuario(nome, email, senha));
+		} else {
+			System.out.println("Senha incorreta! ");
+		}
+		System.out.println("Cadastro realizado com sucesso! ");
+		System.out.println(usuario);
+
+		sc.close();
+	}
+
+}
