83647881673d307a56a9542c8720189c04a4e381
Bruno <brunovieirabow@gmail.com>
2021-08-10 22:29:21 -0300

    Tratamento de erros.

--- sistemaCadastro/src/application/Program.java
+++ sistemaCadastro/src/application/Program.java
@@ -15,73 +15,86 @@ public class Program {
 	}
 
 	public static void opcoes() {
-		System.out.println("1=Realizar cadastro:\n2=Realizar login:");
-		Integer op = 0;
-		do {
-			op = sc.nextInt();
-			sc.nextLine();
+		try {
+			System.out.println("1=Realizar cadastro:\n2=Realizar login:");
+			String op;
+			do {
+				op = sc.nextLine();
 
-			switch (op) {
-			case 1:
-				cadastro();
-				break;
-			case 2:
-				login();
-				break;
+				switch (op) {
+				case "1":
+					cadastro();
+					break;
+				case "2":
+					login();
+					break;
 
-			default:
+				default:
+					System.out.println("Informe uma operação válida.");
+					break;
+				}
 
-				break;
-			}
-		} while (true);
+			} while (true);
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro inesperado, tente novamente.");
+
+		}
+
 	}
 
 	public static void cadastro() {
+		try {
+			String senha;
+			String senhaVal;
+			System.out.print("Bem vindo! ");
 
-		String senha;
-		String senhaVal;
-		System.out.print("Bem vindo! ");
+			System.out.print("Informe o seu nome: ");
+			String nome = sc.nextLine();
+			System.out.print("Informe o seu Email: ");
+			String email = sc.nextLine();
 
-		System.out.print("Informe o seu nome: ");
-		String nome = sc.nextLine();
-		System.out.print("Informe o seu Email: ");
-		String email = sc.nextLine();
+			System.out.print("Digite a sua senha: ");
+			senha = sc.nextLine();
+			do {
+				System.out.print("Confirme a senha: ");
+				senhaVal = sc.nextLine();
 
-		System.out.print("Digite a sua senha: ");
-		senha = sc.nextLine();
-		do {
-			System.out.print("Confirme a senha: ");
-			senhaVal = sc.nextLine();
+				if (senhaVal.equals(senha)) {
+					list.add(new Usuario(nome, email, senha));
+				} else {
+					System.out.println("Senha incorreta! ");
 
-			if (senhaVal.equals(senha)) {
-				list.add(new Usuario(nome, email, senha));
-			} else {
-				System.out.println("Senha incorreta! ");
-
-			}
-		} while (senhaVal.equals(senha) != true);
-		System.out.println("Cadastro realizado com sucesso! ");
-		System.out.println(list);
+				}
+			} while (senhaVal.equals(senha) != true);
+			System.out.println("Cadastro realizado com sucesso! ");
+			System.out.println(list);
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro inesperado, tente novamente.");
+		}
 
 	}
 
 	public static void login() {
+		try {
+			boolean aux = false;
+			do {
+				System.out.print("Informe o nome de usuário: ");
+				String nome = sc.nextLine();
+				System.out.print("Informe a senha: ");
+				String senha = sc.nextLine();
 
-		boolean aux = false;
-		do {
-			System.out.print("Informe o nome de usuário: ");
-			String nome = sc.nextLine();
-			System.out.print("Informe a senha: ");
-			String senha = sc.nextLine();
+				for (Usuario usuario : list) {
+					if (nome.equals(usuario.getNome()) == false || senha.equals(usuario.getSenha()) == false) {
+					} else
+						aux = true;
 
-			for (Usuario usuario : list) {
-				if (nome.equals(usuario.getNome()) == false || senha.equals(usuario.getSenha()) == false) {
-				} else
-					aux = true;
+				}
+			} while (aux == false);
+			System.out.println("Login efetuado com sucesso! ");
+		} catch (Exception e) {
+			System.out.println("Ocorreu um erro inesperado, tente novamente.");
 
-			}
-		} while (aux == false);
-		System.out.println("Login efetuado com sucesso! ");
+		}
 
 	}
 
